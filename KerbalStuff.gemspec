38796bca013e670fbce5fe8c025da14bb329f968
Alexandre Oliveira <rockytvbr@gmail.com>
2014-09-19 20:22:53 -0300

    Updated to 0.1.3
    Added two new methods - GetBasicModInfo(id) and GetBasicUserInfo(string)

--- KerbalStuff.gemspec
+++ KerbalStuff.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 	s.name		= 'KerbalStuff'
-	s.version	= '0.1.2'
-	s.date 		= '2014-09-17'
+	s.version	= '0.1.3'
+	s.date 		= '2014-09-19'
 	s.summary 	= "KerbalStuff"
 	s.description = "A simple API wrapper for KerbalStuff"
 	s.authors	= ["Alexandre Oliveira"]
--- lib/KerbalStuff.rb
+++ lib/KerbalStuff.rb
@@ -11,7 +11,7 @@ module KerbalStuff
 	MOD = "https://kerbalstuff.com/api/mod/"
 	
 	def self.get_https_response(url)
-		@url = URI.parse(url)
+		@url = URI.parse(URI.escape(url))
 		http = Net::HTTP.new(@url.host, @url.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -25,7 +25,7 @@ module KerbalStuff
 	# Searches for mods with the specified keyword/phrase.
 	#
 	# @param query [String] the keyword/phrase to search for.
-	# @return [String] A parsed JSON output containing the mods which were found.
+	# @return [Hash] A parsed JSON output containing the mods which were found.
 	def self.SearchMod(query)
 		response = get_https_response("#{SEARCH_MOD}#{query}")
 		JSON.parse(response.body)
@@ -34,7 +34,7 @@ module KerbalStuff
 	# Searches for users with the specified keyword/phrase.
 	#
 	# @param query [String] the keyword/phrase to search for.
-	# @return [String] A parsed JSON output containing the users which were found.
+	# @return [Hash] A parsed JSON output containing the users which were found.
 	def self.SearchUser(query)
 		response = get_https_response("#{SEARCH_USER}#{query}")
 		JSON.parse(response.body)
@@ -43,7 +43,7 @@ module KerbalStuff
 	# Retrieves information about the specified user.
 	#
 	# @param username [String] the username to retrieve its information.
-	# @return [String] A parsed JSON output containing the information about the user.
+	# @return [Hash] A parsed JSON output containing the information about the user.
 	def self.User(username)
 		raise "username cannot be nil" unless username.length > 0
 		
@@ -54,7 +54,7 @@ module KerbalStuff
 	# Retrieves the information about the specified mod.
 	#
 	# @param id [Fixnum] the id to retrieve its information.
-	# @return [String] A parsed JSON output containing the information about the mod.
+	# @return [Hash] A parsed JSON output containing the information about the mod.
 	def self.Mod(id)
 		raise "id cannot be nil" unless id.is_a?(Integer) and id > 0
 		
@@ -65,12 +65,46 @@ module KerbalStuff
 	# Retrieves the information about the slast released version of the specified mod.
 	#
 	# @param username [Fixnum] the id to retrieve information of its latest version released.
-	# @return [String] A parsed JSON output containing the information about the latest version released.
+	# @return [Hash] A parsed JSON output containing the information about the latest version released.
 	def self.GetLatestVersion(id)
 		raise "id cannot be nil" unless id.is_a?(Integer) and id > 0
 		
 		response = get_https_response("#{MOD}#{id}/latest")
 		JSON.parse(response.body)
+	end
+	
+	# Retrieves basic mod information - name, author, description, how many times it was downloaded, url to the mod and latest version.
+	#
+	# @param id [Fixnum] The id of the mod to retrieve information for.
+	# @return [Hash] A hash containing basic mod info.
+	def self.GetBasicModInfo(id)
+		oldHash = Mod(id)
+		
+		modHash = Hash.new()
+		modHash["name"] = oldHash["name"]
+		modHash["author"] = oldHash["author"]
+		modHash["description"] = oldHash["short_description"]
+		modHash["downloads"] = oldHash["downloads"]
+		modHash["url"] = "https://kerbalstuff.com/mod/#{id}/}"
+		modHash["latest_version"] = oldHash["versions"][0]["friendly_version"]
+		
+		modHash
+	end
+	
+	# Retrieves basic user info - username, mods, irc nick and forum username.
+	#
+	# @param username [String] The user to gather information for.
+	# @return [Hash] A hash containing the basic user info.
+	def self.GetBasicUserInfo(string)
+		oldHash = User(string)
+		
+		userHash = Hash.new()
+		userHash["username"] = oldHash["username"]
+		userHash["mods"] = oldHash["mods"]
+		userHash["ircNick"] = oldHash["ircNick"]
+		userHash["forumUsername"] = oldHash["forumUsername"]
+		
+		userHash
 	end
 	
 end
