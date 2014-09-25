8d94ee9ae39fc5fca7938ff20574ce0f7cd2f69c
Alexandre Oliveira <rockytvbr@gmail.com>
2014-09-25 14:16:44 -0300

    Updated to 0.1.5
    Contains a lot of changes, specially one that adds a new method to make
    my life easier. Thanks to SirCmpwn, the KerbalStuff creator, for adding
    a json error page if the params are kind of invalid.

--- KerbalStuff.gemspec
+++ KerbalStuff.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 	s.name		= 'KerbalStuff'
-	s.version	= '0.1.4'
-	s.date 		= '2014-09-22'
+	s.version	= '0.1.5'
+	s.date 		= '2014-09-25'
 	s.summary 	= "KerbalStuff"
 	s.description = "A simple API wrapper for KerbalStuff"
 	s.authors	= ["Alexandre Oliveira"]
--- lib/KerbalStuff.rb
+++ lib/KerbalStuff.rb
@@ -5,8 +5,6 @@ require 'uri'
 module KerbalStuff
 
 	api_url = "https://kerbalstuff.com/api"
-	search_mod = "https://kerbalstuff.com/api/search/mod?query="
-	search_user = "https://kerbalstuff.com/api/search/user?query="
 	user_url = "https://kerbalstuff.com/api/user/"
 	mod_url = "https://kerbalstuff.com/api/mod/"
 	
@@ -22,44 +20,77 @@ module KerbalStuff
 		response
 	end
 	
+	def self.get_json(url)
+		response = get_https_response(url)
+		json = JSON.parse(response.body)
+		
+		if json.is_a?(Hash)
+			if json.has_key? 'error'
+				return "error", "#{json['reason']}"
+			else
+				return json
+			end
+		end
+	end
+	
 	# Searches for mods with the specified keyword/phrase.
 	#
 	# @param query [String] the keyword/phrase to search for.
-	# @return [Hash] A parsed JSON output containing the mods which were found.
+	# @return [Hash] A parsed JSON output containing the mods which were found. Will return a [String] if no results were found.
 	def self.search_mod(query)
-		response = get_https_response("#{search_mod}#{query}")
-		JSON.parse(response.body)
+		res = get_json("https://kerbalstuff.com/api/search/mod?query=#{query}")
+		
+		if res.length == 0
+			return "No results were found for '#{query}'."
+		else
+			return res
+		end
 	end
 	
 	# Searches for users with the specified keyword/phrase.
 	#
 	# @param query [String] the keyword/phrase to search for.
-	# @return [Hash] A parsed JSON output containing the users which were found.
+	# @return [Hash] A parsed JSON output containing the users which were found. Will return a [String] if no results were found.
 	def self.search_user(query)
-		response = get_https_response("#{search_user}#{query}")
-		JSON.parse(response.body)
+		res = get_json("https://kerbalstuff.com/api/search/user?query=#{query}")
+		
+		if res.length == 0
+			return "No results were found for '#{query}'."
+		else
+			return res
+		end
 	end
 	
 	# Retrieves information about the specified user.
 	#
 	# @param username [String] the username to retrieve its information.
-	# @return [Hash] A parsed JSON output containing the information about the user.
+	# @return [Hash] A parsed JSON output containing the information about the user. Will return a [String] if no users were found.
 	def self.user(username)
 		raise "username cannot be nil" unless username.length > 0
 		
-		response = get_https_response("#{user_url}#{username}")
-		JSON.parse(response.body)
+		res = get_json("https://kerbalstuff.com/api/user/#{username}")
+		
+		if res.include? 'error'
+			return res[1]
+		else
+			return res
+		end
 	end
 
 	# Retrieves the information about the specified mod.
 	#
 	# @param id [Fixnum] the id to retrieve its information.
-	# @return [Hash] A parsed JSON output containing the information about the mod.
+	# @return [Hash] A parsed JSON output containing the information about the mod. Will return a [String] if no mods were found.
 	def self.mod(id)
-		raise "id cannot be nil" unless id.is_a?(Integer) and id > 0
+		raise "id cannot be nil" unless id.is_a?(Integer)
 		
-		response = get_https_response("#{mod_url}#{id}")
-		JSON.parse(response.body)
+		res = get_json("https://kerbalstuff.com/api/mod/#{id}")
+		
+		if res.include? 'error'
+			return res[1]
+		else
+			return res
+		end
 	end
 	
 	# Retrieves the information about the last released version of the specified mod.
@@ -67,10 +98,15 @@ module KerbalStuff
 	# @param username [Fixnum] the id to retrieve information of its latest version released.
 	# @return [Hash] A parsed JSON output containing the information about the latest version released.
 	def self.get_latest_version(id)
-		raise "id cannot be nil" unless id.is_a?(Integer) and id > 0
+		raise "id cannot be nil" unless id.is_a?(Integer)
 		
-		response = get_https_response("#{mod}#{id}/latest")
-		JSON.parse(response.body)
+		res = get_json("https://kerbalstuff.com/api/mod/#{id}/latest")
+		
+		if res.include? 'error'
+			return res[1]
+		else
+			return res
+		end
 	end
 	
 	# Retrieves basic mod information - name, author, description, how many times it was downloaded, url to the mod and latest version.
@@ -80,31 +116,39 @@ module KerbalStuff
 	def self.get_basic_mod_info(id)
 		oldHash = mod(id)
 		
-		modHash = Hash.new()
-		modHash["name"] = oldHash["name"]
-		modHash["author"] = oldHash["author"]
-		modHash["description"] = oldHash["short_description"]
-		modHash["downloads"] = oldHash["downloads"]
-		modHash["url"] = "https://kerbalstuff.com/mod/#{id}/}"
-		modHash["latest_version"] = oldHash["versions"][0]["friendly_version"]
-		
-		modHash
+		if oldHash.is_a?(String)
+			return oldHash
+		else
+			modHash = Hash.new()
+			modHash["name"] = oldHash["name"]
+			modHash["author"] = oldHash["author"]
+			modHash["description"] = oldHash["short_description"]
+			modHash["downloads"] = oldHash["downloads"]
+			modHash["url"] = "https://kerbalstuff.com/mod/#{id}/}"
+			modHash["latest_version"] = oldHash["versions"][0]["friendly_version"]
+			
+			return modHash
+		end
 	end
 	
 	# Retrieves basic user info - username, mods, irc nick and forum username.
 	#
 	# @param username [String] The user to gather information for.
 	# @return [Hash] A hash containing the basic user info.
-	def self.get_basic_user_info(string)
-		oldHash = user(string)
+	def self.get_basic_user_info(username)
+		oldHash = user(username)
 		
-		userHash = Hash.new()
-		userHash["username"] = oldHash["username"]
-		userHash["mods"] = oldHash["mods"]
-		userHash["ircNick"] = oldHash["ircNick"]
-		userHash["forumusername"] = oldHash["forumusername"]
+		if oldHash.is_a?(String)
+			return oldHash
+		else
+			userHash = Hash.new()
+			userHash["username"] = oldHash["username"]
+			userHash["mods"] = oldHash["mods"]
+			userHash["ircNick"] = oldHash["ircNick"]
+			userHash["forumusername"] = oldHash["forumusername"]
 		
-		userHash
+			return userHash
+		end
 	end
 	
 end
